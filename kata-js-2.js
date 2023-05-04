4d97c20eb6dd23de4a726fe50c469c942e63a43d
Mariangelica Rodriguez <gromarant@gmail.com>
2023-05-04 10:48:16 +0200

    Fixed exercise

--- kata-js-2.js
+++ kata-js-2.js
@@ -3,11 +3,14 @@ Escriba un programa que pida un año y que escriba si es bisiesto o no.
 Se recuerda que los años bisiestos son múltiplos de 4, pero los múltiplos de 100 no lo son, aunque los múltiplos de 400 sí.*/
 
 const leapYear = (year) => {
-  if(year % 4 === 0|| year % 400  === 0) {
-    return `${year}, es un año bisiesto`;
-  } else {
+  if (year % 4 !== 0 || year % 100 === 0 && year % 400 !== 0) { 
     return `${year}, no es año bisiesto`;
+  } else {
+    return `${year}, es un año bisiesto`;
   }
 }
 
-console.log(leapYear(2009));
+console.log(leapYear(100));
+console.log(leapYear(4));
+console.log(leapYear(400));
+console.log(leapYear(1991));
